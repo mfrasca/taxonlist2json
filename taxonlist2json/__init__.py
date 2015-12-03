d79bb66a0dddc83f38e112bc463b19dadbcb7fc2
angelchavez <angel46_8@hotmail.com>
2015-12-03 10:30:00 -0800

    Corregido el Error

--- taxonlist2json/__init__.py
+++ taxonlist2json/__init__.py
@@ -8,7 +8,9 @@ def line_to_binomial(input):
     values = input.strip().split(' ', 2)
     result['ht-epithet'] = values[0]
     result['epithet'] = values[1]
-    result['author'] = values[2]
+    import HTMLParser
+    var = HTMLParser.HTMLParser()
+    result['author'] = var.unescape(values[2])
 
     return result
 
