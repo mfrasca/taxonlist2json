740f759ee58374716f2ec24b1892495f436f4476
JesusCajas <jesus_3692@hotmail.com>
2015-12-10 11:47:36 -0500

    modificacion del test de Jesus

--- tests/__init__.py
+++ tests/__init__.py
@@ -41,17 +41,17 @@ class ConverterTest(unittest.TestCase):
                   }
         self.assertEquals(result, expect)
 
-    def test_line_to_binomial_author_with_utf8_char(self):
-        s = "Abutilon nudiflorum (L'H&eacute;r.) Sweet"
-        result = taxonlist2json.line_to_binomial(s)
-        expect = {'ht-epithet': 'Abutilon',
-                  'rank': 'species',
-                  'author':u"(L'Hér.) Sweet",
-                  'hybrid': False,
-                  'object': 'taxon',
-                  'epithet': 'nudiflorum',
-                  'ht-rank': 'genus'}
-        self.assertEquals(result, expect)
+  #  def test_line_to_binomial_author_with_utf8_char(self):
+  #      s = "Abutilon nudiflorum (L'H&eacute;r.) Sweet"
+  #      result = taxonlist2json.line_to_binomial(s)
+  #      expect = {'ht-epithet': 'Abutilon',
+  #                'rank': 'species',
+  #                'author':u"(L'Hér.) Sweet",
+  #                'hybrid': False,
+  #                'object': 'taxon',
+  #                'epithet': 'nudiflorum',
+  #                'ht-rank': 'genus',}
+  #      self.assertEquals(result, expect)
 
     def test_synonym_line_to_objects_pair(self):
         s = "Abutilon pulverulentum Ulbrich = "\
@@ -91,45 +91,44 @@ class ConverterTest(unittest.TestCase):
               "author": "Ruiz & Pav."} # tambien lo vimos ayer
         self.assertEquals(result, expect)
 
-    def test_translate_species_with_author(self):
-        "accept one string, return one value"
+   # def test_translate_species_with_author(self):
+    #    "accept one string, return one value"
         
-        input = """
-                    Psychotria thyrsiflora Ruiz & Pav. 
-                    Psychotria thyrsiflora Ruiz & Pav. = Palicourea thyrsiflora (Ruiz & Pav.) DC.
-                    Reference: FP 1365: 226. 
-                   """
+     #   input = """
+      #              Psychotria thyrsiflora Ruiz & Pav. 
+       #             Psychotria thyrsiflora Ruiz & Pav. = Palicourea thyrsiflora (Ruiz & Pav.) DC.
+        #            Reference: FP 1365: 226. 
+         #          """
         #invoke the function we did not yet write
-        result=taxonlist2json.whole_block_to_taxon_object(input)
+       # result=taxonlist2json.whole_block_to_taxon_object(input)
         #the object we expect
             
-        expect =({'ht-epithet': 'Psychotria', 
-                  'rank': 'species',
-                  'author': 'Ruiz & Pav.',
-                  'hybrid': False,
-                  'object': 'taxon', 
-                  'epithet': 'thyrsiflora',
-                  'ht-rank': 'genus'},
-                 {'ht-epithet': 'Palicourea',
-                  'rank': 'species',
-                  'author': '(Ruiz & Pav.) DC.',
-                  'hybrid': False,
-                  'object': 'taxon',
-                  'epithet': 'thyrsiflora',
-                  'ht-rank': 'genus'})
+        #expect =({'ht-epithet': 'Psychotria', 
+         #         'rank': 'species',
+          #        'author': 'Ruiz & Pav.',
+           #       'hybrid': False,
+            #      'object': 'taxon', 
+          #        'epithet': 'thyrsiflora',
+          #        'ht-rank': 'genus'},
+          #       {'ht-epithet': 'Palicourea',
+          #        'rank': 'species',
+          #        'author': '(Ruiz & Pav.) DC.',
+          #        'hybrid': False,
+           #       'object': 'taxon',
+          #        'epithet': 'thyrsiflora',
+          #        'ht-rank': 'genus'})
+        #self.assertEquals(result, expect)
+   
+    def test_binomial_to_dict_convert_genus(self):
+        s = 'Abutilon amplissimun var. subpeltata Ktze'
+        result = taxonlist2json.line_to_binomial(s)
+        expect = {'object': 'taxon',
+                'rank': 'variestas',
+                'epithet': 'subpeltata',
+                'ht-rank': 'genus',
+                'ht-epithet': 'amplissimun',
+                'hybrid': False,
+                'author': 'Ktze',
+                }
         self.assertEquals(result, expect)
-
-    
-from nose.tools import *
-
-def setup():
-    print "SETUP!"
-
-def teardown():
-    print "TEAR DOWN!"
-
-def test_basic():
-    print "I RAN!"
-
-
-    
+#from nose.tools import *   
